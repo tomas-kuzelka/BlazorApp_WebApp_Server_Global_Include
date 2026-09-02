c7ba2e1498f415398be9c3308560d4920fd06e57
Tomas Kuzelka <tkuzelka@gmail.com>
2026-09-02 19:28:28 +0200

    Initial commit

---
+++ BlazorApp_WebApp_Server_Global_Include/Code/SharedCounter.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp_WebApp_Server_Global_Include.Code;
+
+public class SharedCounter
+{
+    private int _count;
+    public int Count => _count;
+    // událost, která se vyvolá při změně počtu
+    // událost je typu Action, což znamená, že nevrací žádnou hodnotu a nemá žádné parametry
+    public event Action? CountChanged;
+    public void Increment()
+    {
+        // _count++;
+        // lepší řešení pro thread safety
+        Interlocked.Increment(ref _count);
+        CountChanged?.Invoke();
+    }
+}
---
+++ BlazorApp_WebApp_Server_Global_Include/Program.cs
@@ -0,0 +1,27 @@
+using BlazorApp_WebApp_Server_Global_Include.Code;
+using BlazorApp_WebApp_Server_Global_Include.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Add the SharedCounter service as a singleton
+builder.Services.AddSingleton<SharedCounter>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
